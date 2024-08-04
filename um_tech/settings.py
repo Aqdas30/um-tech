15e1d73fab583b9c4963d31bd925d11c589ace97
unknown <aqdaszulfiqar30@gmail.com>
2024-08-04 18:20:36 +0500

    commit

--- um_tech/settings.py
+++ um_tech/settings.py
@@ -96,8 +96,11 @@ WSGI_APPLICATION = 'um_tech.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default='postgres://localhost/mydatabase'
+    )
 }
+
 
 
 # Password validation
