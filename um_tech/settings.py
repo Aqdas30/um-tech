b10e709b28639ddc593131bcdaaf6a229041b4e1
unknown <aqdaszulfiqar30@gmail.com>
2024-08-04 18:08:50 +0500

    Configured database to use Heroku's DATABASE_URL

--- um_tech/settings.py
+++ um_tech/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'um_tech.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost/mydb')
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
